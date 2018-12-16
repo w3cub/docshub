c1dd50b528b96b1e5e4fd317c3101c78438ba66f
Terry Cai <gidcai@gmail.com>
2018-12-16 11:36:52 +0800

    test travis

--- deploy/initdev.sh
+++ deploy/initdev.sh
@@ -1,8 +1,8 @@
-cd ./devdocs 
+cd ./devdocs
+bundle install
 thor docs:download --default # download all file
 
 cd ..
 cd ./docslogo
-sudo apt-get -y install imagemagick graphicsmagick
 npm install -d
 gulp beauty
