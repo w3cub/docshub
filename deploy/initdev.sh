fcccc7f5ad3f2c07aebe312663c644245c6c1b12
Terry Cai <gidcai@gmail.com>
2018-12-16 10:42:16 +0800

    update scripts

---
+++ deploy/initdev.sh
@@ -0,0 +1,8 @@
+cd ./devdocs 
+thor docs:download --default # download all file
+
+cd ..
+cd ./docslogo
+sudo apt-get -y install imagemagick graphicsmagick
+npm install -d
+gulp beauty
---
+++ deploy/release.sh
@@ -0,0 +1,12 @@
+
+cd ./devdocs 
+thor docs:download --all # download all file
+cd ..
+cd ./docslogo
+sudo apt-get install imagemagick graphicsmagick
+npm install -d
+gulp beauty
+
+cd ./website
+rake setup_gen # setup generate queue
+rake multi_gen_deploy # project release
